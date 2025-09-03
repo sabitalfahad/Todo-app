fb17bbdebc0aaccbaf3ebc3707419db259919302
Sikder Sabit Al Fahad <sksabit11@gmail.com>
2025-09-03 13:23:06 +0600

    __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -5,10 +5,10 @@ This package provides a simple Todo list application with GUI support.
 Exports the TodoApp class as the main interface.
 """
 
-from .functions import TodoApp
+from functions import TodoApp
 
 __version__ = "1.0.0"
-__author__ = "Your Name"
+__author__ = "Sikder Sabi Al Fahad"
 
 # Export TodoApp as the main interface
 __all__ = ["TodoApp"]
